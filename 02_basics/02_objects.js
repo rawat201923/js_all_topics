bdb0e522ea4b869cd25b0f654d02b69ef23c1289
rawat201923 <140051821+rawat201923@users.noreply.github.com>
2024-05-02 12:31:26 +0000

    DONE FOR TODAY

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -66,5 +66,14 @@ const obj1 = {1:"a", 2: "b"}
 const obj2 = {3:"a", 4: "b"}
 
 // const obj3 = {obj1, obj2}
-const obj3 = Object.assign(obj1, obj2) //Object.assign(target,source)
-console.log(obj3)
+// const obj3 = Object.assign(obj1, obj2) //Object.assign(target,source)
+
+const obj3= {...obj1, ...obj2} //mosly used way
+// console.log(obj3);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
